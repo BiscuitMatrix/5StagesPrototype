ba2da2c308f710dd5ff7c6d8cc7e31a02d9eaae2
GeorgeGerald <slowpokegerald95@gmail.com>
2017-10-23 11:45:38 +0100

    Update

---
+++ 5 Stages Prototype/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+
+    // Variable Declarations
+    int constVel;
+
+    // Function Declarations
+    //void autoMove();
+    //void input();
+
+
+    // Use this for initialization
+    void Start()
+    {
+        constVel = 2;
+        //autoMove();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void autoMove()
+    {
+        //vel.x = constVel;
+    }
+
+    void input()
+    {
+        // Detect and remember input
+        // How do you represent a swipe in code?
+        // Take the current position
+        // Take the past position
+        // Compare
+
+        if (Input.GetTouch(0) == /* Swipe Left */)
+        {
+            // Is there a path on the left?
+            // If there is, move player to path on the left
+            // If not, do nothing
+        }
+        else if ((Input.GetTouch(0) == /* Swipe Right */))
+        {
+            // Is there a path on the right?
+            // If there is, move player to path on the right
+            // If not, do nothing
+        }
+    } // End Input
+
+}
