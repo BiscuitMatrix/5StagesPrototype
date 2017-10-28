536486c5100c7a27dc39a2181c0b5835c89a0713
AGuild <guilder225@gmail.com>
2017-10-28 20:30:49 +0100

    MetaGame
    Rotation Buttons

---
+++ 5 Stages Prototype/Assets/Scripts/Meta Game/Rotate_On_Touch.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate_On_Touch : MonoBehaviour
+{
+
+    public GameObject Planet;
+
+    public float rotSpeed;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (SystemInfo.deviceType == DeviceType.Handheld)
+        {
+            if (Input.touchCount > 0)
+            {
+                RaycastHit hitMe = new RaycastHit();
+                bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.GetTouch(0).deltaPosition), out hitMe);
+
+                if (hit)
+                {
+                    Debug.Log("Hit" + hitMe.transform.gameObject.name);
+
+                    if (hitMe.transform.gameObject.name == "Left")
+                    {
+                        Planet.transform.Rotate(Vector3.up, rotSpeed);
+                    }
+                    else if (hitMe.transform.gameObject.name == "Right")
+                    {
+                        Planet.transform.Rotate(Vector3.up, -rotSpeed);
+                    }
+                }
+            }
+        }
+        else if (SystemInfo.deviceType == DeviceType.Desktop)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                RaycastHit hitMe = new RaycastHit();
+                bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitMe);
+
+                if (hit)
+                {
+                    Debug.Log("Hit" + hitMe.transform.gameObject.name);
+
+                    if (hitMe.transform.gameObject.name == "Left")
+                    {
+                        Planet.transform.Rotate(Vector3.up, rotSpeed);
+                    }
+                    else if (hitMe.transform.gameObject.name == "Right")
+                    {
+                        Planet.transform.Rotate(Vector3.up, -rotSpeed);
+                    }
+
+                }
+            }
+        }
+
+
+    }
+}
