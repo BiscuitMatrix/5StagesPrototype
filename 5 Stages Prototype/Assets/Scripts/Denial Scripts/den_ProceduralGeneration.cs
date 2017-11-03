16bb95096b7123b7a120d4e412498a4773173ea9
GeorgeGerald <slowpokegerald95@gmail.com>
2017-11-03 20:40:16 +0000

    Code Updates
    More pseudocode, and updates for the player input

---
+++ 5 Stages Prototype/Assets/Scripts/Denial Scripts/den_ProceduralGeneration.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class den_ProceduralGeneration : MonoBehaviour 
+{
+	// Use this for initialization
+	void Start () 
+	{
+		// Setup how new blocks are going to enter the game
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// When an old set of blocks is out of view
+			// 1. Delete the Old Blocks
+
+			// 2. Instantiate new blocks into the scene
+		
+	}
+
+	void spawnPath () 
+	{
+		var spawn = (var)Instantiate();
+	}
+}
