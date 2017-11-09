ea467505e8f7e30d7a37cd183ed4efde7a418835
1501120-Peter <1501120@abertay.ac.uk>
2017-11-09 11:53:43 +0000

    Anger Stage - First Commit (Scripts and Base Objects)
    CameraController
    Controller
    LightningPowerPlacer
    LightningPowerScript
    WallBreakScript
    
    GROUND, PLAYER, LIGHTNINGPLACER, LIGHTNINGBOLT
    
    Scene layout edited.

---
+++ 5 Stages Prototype/Assets/Scripts/Anger Scripts/Controller.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Controller : MonoBehaviour
+{
+
+    // Initialise variables
+    public float speed = 1000;
+    public float bonusSpeed = 10;
+    public float velocity;
+
+    private Rigidbody rb;
+    private GameObject wall;
+    private WallBreakScript wallBreakScript;
+    private Vector3 lastVelocity;
+
+
+    void Start()
+    {
+        // Get the player's RigidBody
+        rb = GetComponent<Rigidbody>();
+
+        // Lock the screen orientation to portrait
+        Screen.orientation = ScreenOrientation.Portrait;
+    }
+
+    void Main()
+    {
+        // Prevent phone from sleeping
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+
+    }
+
+    private void Update()
+    {
+        // Exit condition for mobile
+        if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Application.Quit();
+            }
+
+        velocity = rb.velocity.sqrMagnitude;
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Calculate movement vector
+        Vector3 movement = Vector3.zero;
+        movement.x = Input.acceleration.x;
+        //movement.z = Input.acceleration.y;
+
+        
+        // Clamp the movement vector
+        if (movement.sqrMagnitude > 1)
+        {
+            movement.Normalize();
+        }
+        
+        // Move the player based on the tilt
+        movement *= Time.deltaTime;
+
+        rb.AddForce(movement*speed, ForceMode.Acceleration);
+        rb.AddForce(new Vector3(0, 0, 1), ForceMode.Force);
+
+        lastVelocity = rb.velocity;
+    }
+
+    // Handle Collisions
+    void OnCollisionEnter(Collision col)
+    {
+        // If colliding with a Breakable object
+        if(col.gameObject.tag == "Breakable")
+        {
+            wall = col.gameObject;
+            wallBreakScript = wall.GetComponent<WallBreakScript>();
+            // Compare the player's speed against the break speed required for the wall
+            if (lastVelocity.magnitude > wallBreakScript.break_Speed)
+            {
+                // If player is fast enough, destroy the wall
+                Destroy(col.gameObject);
+                rb.AddForce(new Vector3(lastVelocity.x * bonusSpeed, 0.0f, lastVelocity.z * bonusSpeed), ForceMode.Impulse);
+            }
+            else
+            {
+                SceneManager.LoadScene("main");
+            }
+        }
+        if (col.gameObject.tag == "Goal")
+        {
+            if (lastVelocity.magnitude > wallBreakScript.break_Speed)
+            {
+                SceneManager.LoadScene("main");
+            }
+        }
+    }
+
+}
---
+++ 5 Stages Prototype/Assets/Scripts/Anger Scripts/LightningPowerPlacer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningPowerPlacer : MonoBehaviour {
+
+    public GameObject ground;
+    public GameObject player;
+    public GameObject powerup;
+    public float minPlaceDist;
+    public float maxPlaceDist;
+    public int strikeTimer;
+    public int strikeLength;
+
+    private Vector3 powerupPos;
+    private int counter = 0;
+
+    void RandomisePos()
+    {
+        Renderer groundRenderer = ground.GetComponent<Renderer>();
+        powerupPos = new Vector3(Random.Range((ground.transform.position.x - (groundRenderer.bounds.size.x/2)), (ground.transform.position.x + (groundRenderer.bounds.size.x / 2))),
+                                 ground.transform.position.y + 1.0f,
+                                 Random.Range(player.transform.position.z + minPlaceDist, (player.transform.position.z + maxPlaceDist)));
+    }
+
+    // Use this for initialization
+    void Start () {
+        powerup.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(counter == strikeTimer * 60)
+        {
+            RandomisePos();
+            powerup.transform.position = powerupPos;
+            powerup.SetActive(true);
+        }
+        if(counter == ((strikeTimer + strikeLength) * 60))
+        {
+            powerup.SetActive(false);
+            counter = 0;
+        }
+        counter++;
+	}
+}
