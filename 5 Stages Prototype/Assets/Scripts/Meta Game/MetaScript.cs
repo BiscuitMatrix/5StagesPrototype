a54dcebbac8a18a086473f58fbb73854afae7bd9
AGuild <guilder225@gmail.com>
2017-11-11 16:28:07 +0000

    MetaGame/Shop
    Rotation from world to shop implemented

--- 5 Stages Prototype/Assets/Scripts/Meta Game/MetaScript.cs
+++ 5 Stages Prototype/Assets/Scripts/Meta Game/MetaScript.cs
@@ -7,6 +7,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+//TO DO
+//- FIX rotateToShop, rotateToMain functions
+//- remove Desktop Code
+
 public class MetaScript : MonoBehaviour
 {
     public GameObject Planet;
@@ -16,7 +20,8 @@ public class MetaScript : MonoBehaviour
     public Transform lookShop;
     public Transform lookMain;
 
-    private const float cameraRotSpeed = 0.1f;
+    private bool isCoRoutineActive = false;
+    Coroutine lastRoutine = null;
 
     void Start()
     {
@@ -25,23 +30,25 @@ public class MetaScript : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButton(0))        //change to Input.TouchCount > 0;
+        if (SystemInfo.deviceType == DeviceType.Handheld) //Check if Handheld device
         {
-            RaycastHit HitDestination = new RaycastHit();
+            if (Input.touchCount > 0)        //check for a touch
+            {
+                RaycastHit HitDestination = new RaycastHit();
 
-            bool Hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out HitDestination);      //change Input.mousePosition to Input.GetTouch(0).deltaPosition
+                bool Hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.GetTouch(0).deltaPosition), out HitDestination);      //get touch position for raycast
 
-            switch (HitDestination.transform.gameObject.name)       //switch statement
-               {
+                switch (HitDestination.transform.gameObject.name)       //switch statement
+                {
                     case "Left":
                         Planet.transform.Rotate(Vector3.up, -rotSpeed);     //roatate planet left
                         break;
-                    
+
 
                     case "Right":
                         Planet.transform.Rotate(Vector3.up, rotSpeed);      //rotate planet right
                         break;
-        
+
                     case "Denial":
                         SceneManager.LoadScene("Denial");       //load Denial Level      
                         break;
@@ -63,30 +70,93 @@ public class MetaScript : MonoBehaviour
                         break;
 
                     case "Shop":
-                        rotateToShop();
+                        StartCoroutine(rotateToShop());
                         break;
 
                     case "Back":
-                        rotateToMain();
+                        StartCoroutine(rotateToMain());
                         break;
-               }
-           }
-       }
+                }
+            }
+        }
+        else if (SystemInfo.deviceType == DeviceType.Desktop)       //REMOVE AFTER TESTING ON DESKTOP
+        {
+            if (Input.GetMouseButton(0))        //change to Input.TouchCount > 0;
+            {
+                RaycastHit HitDestination = new RaycastHit();
 
-    void rotateToShop()
-    {
-       
+                bool Hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out HitDestination);      //change Input.mousePosition to Input.GetTouch(0).deltaPosition
+
+                switch (HitDestination.transform.gameObject.name)       //switch statement
+                {
+                    case "Left":
+                        Planet.transform.Rotate(Vector3.up, -rotSpeed);     //roatate planet left
+                        break;
+
+
+                    case "Right":
+                        Planet.transform.Rotate(Vector3.up, rotSpeed);      //rotate planet right
+                        break;
+
+                    case "Denial":
+                        SceneManager.LoadScene("Denial");       //load Denial Level      
+                        break;
+
+                    case "Anger":
+                        SceneManager.LoadScene("Anger");        //load Anger scene
+                        break;
+
+                    case "Bargaining":
+                        SceneManager.LoadScene("Bargaining");        //load Bargaining scene   
+                        break;
+
+                    case "Depression":
+                        SceneManager.LoadScene("Depression");        //load Depression scene
+                        break;
+
+                    case "Acceptance":
+                        SceneManager.LoadScene("Acceptance");         // Load acceptance scene
+                        break;
+
+                    case "Shop":
+                        if (!isCoRoutineActive)
+                        {
+                            isCoRoutineActive = true;
+                            lastRoutine = StartCoroutine(rotateToShop());
+                        }
+                        break;
+
+                    case "Back":
+                        if (!isCoRoutineActive)
+                        {
+                            isCoRoutineActive = true;
+                            lastRoutine = StartCoroutine(rotateToMain());
+                        }
+                        break;
+                }
+            }
+        }
     }
 
-    void rotateToMain()
+
+    IEnumerator rotateToShop()
     {
-        //if (lookMain != null)
-        //{
-        //    while (cameraTransform.rotation != lookMain.rotation)
-        //    {
-        //        cameraTransform.rotation = Quaternion.Slerp(cameraTransform.rotation, lookMain.rotation, cameraRotSpeed * Time.deltaTime);
-        //    }
-        //}
+        for (float i = 0.0f; i <= 1; i += Time.deltaTime / 2.0f)
+        {
+            cameraTransform.rotation = Quaternion.Slerp(cameraTransform.rotation, lookShop.rotation, i);
+            yield return null;
+        }
+        isCoRoutineActive = false;
+    }
+
+    IEnumerator rotateToMain()
+    {
+        for (float i = 0.0f; i <= 1; i += Time.deltaTime / 2.0f)
+        {
+            cameraTransform.rotation = Quaternion.Slerp(cameraTransform.rotation, lookMain.rotation, i);
+            yield return null;
+        }
+        isCoRoutineActive = false;
     }
     
 }
