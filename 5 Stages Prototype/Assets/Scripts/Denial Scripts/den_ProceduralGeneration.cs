f0b2f4dc6111aa4f931b5e6dea958e0786d3337a
GeorgeGerald <slowpokegerald95@gmail.com>
2017-11-15 23:01:26 +0000

    Adding the denial stage prototype
    Added to master once more

---
+++ 5 Stages Prototype/Assets/Scripts/Denial Scripts/den_ProceduralGeneration.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class den_ProceduralGeneration : MonoBehaviour
+{
+	//Holds the different colors for the path
+	public GameObject [] paths = new GameObject[4];
+	public GameObject player;
+	int counter = 70;
+    float playerZPos;
+
+    // Use this for initialization
+    void Start ()
+	{
+        // Setup an initial path for the player
+		for (int i = 0; i < 70; i++)
+		{
+			for (float width = 0; width < 4; width++)
+			{
+				int holder = (int)width;
+
+				Instantiate(paths[holder], new Vector3(width, 0.0f, i), Quaternion.identity);
+            }
+		}
+	}
+
+	// Update is called once per frame
+	/// <summary>
+    /// 
+    /// </summary>
+    void Update ()
+	{
+        playerZPos = player.GetComponent<Rigidbody>().transform.position.z;
+        //new Vector3 () = player.GetComponent<Transform>();
+        //This sets the limit that the path can draw. Set this to be just outside the camera view
+        
+        if (counter < playerZPos + 70)
+		{
+			//Draw from left to right. Should be updated to sometimes not add to make holes in path
+			for (float width = 0; width < 4; width++)
+			{
+				int holder = (int)width;
+				Instantiate(paths[holder], new Vector3(width, 0.0f, counter), Quaternion.identity);
+			}
+            counter++;
+        }
+        
+		// Clean up path less
+		if(counter % 2 == 0) CleanPath();
+       
+
+        
+        Debug.Log(counter);
+    }
+
+    void CleanPath()
+    {
+        playerZPos = player.GetComponent<Rigidbody>().transform.position.z;
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("den_Path");
+
+        foreach (GameObject Block in objects)
+        {
+            if ((Block.transform.position.z + 3) < playerZPos)
+            {
+                GameObject holder = Block;
+                Destroy(holder, 1);
+            }
+        }
+
+        objects = new GameObject[0];
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
