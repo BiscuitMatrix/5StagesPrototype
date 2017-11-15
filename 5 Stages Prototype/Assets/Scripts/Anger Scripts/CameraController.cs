8a58bd168bdfacf1a99496e2c97122449c8874e4
1501120-Peter <1501120@abertay.ac.uk>
2017-11-15 00:46:16 +0000

    Anger Prototype
    Redone anger prototype

---
+++ 5 Stages Prototype/Assets/Scripts/Anger Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        // Get the distance between the player and the camera, used to keep the distance constant
+        offset = transform.position - player.transform.position;
+	}
+	
+	// LateUpdate is called once per frame after Update
+	void LateUpdate () {
+        // Move with the player
+        transform.position = player.transform.position + offset;
+	}
+}
---
+++ 5 Stages Prototype/Assets/Scripts/Anger Scripts/LightningPowerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningPowerScript : MonoBehaviour {
+
+    public float speedMult = 1.5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            Rigidbody rb = col.GetComponent<Rigidbody>();
+            col.GetComponent<Rigidbody>().AddForce(new Vector3(rb.velocity.x * speedMult, 0.0f, rb.velocity.z * speedMult), ForceMode.Impulse);
+        }
+    }
+}
