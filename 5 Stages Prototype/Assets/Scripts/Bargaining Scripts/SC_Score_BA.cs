b9cb1fca6e0dccd74598859adcd674b230b5b12f
AGuild <guilder225@gmail.com>
2017-11-17 19:42:24 +0000

    Updates

--- 5 Stages Prototype/Assets/Scripts/Bargaining Scripts/SC_Score_BA.cs
+++ 5 Stages Prototype/Assets/Scripts/Bargaining Scripts/SC_Score_BA.cs
@@ -3,20 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SC_Score_BA : MonoBehaviour {
+public class SC_Score_BA : MonoBehaviour
+{
     public Text score;
     private int scoreUpdate;
     private int scoreInt;
     
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 
         scoreInt = 0;
         score.text = "Score:" + scoreInt.ToString();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
 
