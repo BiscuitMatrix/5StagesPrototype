77bfac71bc31873b54b95a49e079ed5cf9cfb61f
GeorgeGerald <slowpokegerald95@gmail.com>
2017-11-19 22:03:10 +0000

    UI Updates

---
+++ 5 Stages Prototype/Assets/Scripts/Denial Scripts/ScoreUpdate_DN.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreUpdate_DN : MonoBehaviour
+{
+	private float score;
+	public GameObject player;
+	public GameObject UI;
+
+	void Start ()
+	{
+		score = player.transform.position.x;
+	}
+
+	void Update ()
+	{
+		AddScore();
+	}
+		
+
+	public void AddScore ()
+	{
+		score = player.transform.position.x;
+		UpdateScore ();
+	}
+
+	void UpdateScore ()
+	{
+		UI.GetComponent<TextAsset> = "Score: " + score;
+	}
+}
